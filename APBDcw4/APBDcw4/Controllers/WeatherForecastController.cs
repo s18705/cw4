f32fc5b0f29af3637a49a2ae4d394538da2418e4
Karolina <61821714+s18705@users.noreply.github.com>
2020-03-31 00:24:58 +0200

    new commit1

---
+++ APBDcw4/APBDcw4/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using APBDcw4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APBDcw4.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+       public IActionResult Get()
+        {
+            SqlConnection con = new SqlConnection("Data Source = bd-mssql;Initial Catalog=S18705;Integrated Security=True");
+
+            //using(var client = new SqlConnection("[CONNECTION STRING]"))
+            //{
+            //
+            //}
+
+            using(var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from Students ....";
+
+                con.Open();
+                var dr = com.ExecuteReader();
+                while(dr.Read())
+                {
+                    var st = new Student();
+                    st.FirstName = dr["FirstName"].ToString();
+                    //...
+                }
+            }
+
+            //SqlCommand com;
+            return Ok();
+        }
+    }
+}
